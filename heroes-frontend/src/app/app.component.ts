f836a6aa49ccfec0818473f8ae248adf67920d69
Christian Köberl <christian.koeberl@porscheinformatik.at>
2017-07-04 08:59:04 +0200

    Implementation for step 3 master/detail

--- heroes-frontend/src/app/app.component.ts
+++ heroes-frontend/src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,8 +20,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero : Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
