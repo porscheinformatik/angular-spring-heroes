ed6d4620b7a6b47216b9486177be1b71c4e4c8f1
Christian Köberl <derkoe@users.noreply.github.com>
2018-05-15 22:21:38 +0200

    Order heroes by name

--- heroes-backend/src/main/java/heroes/backend/HeroRepository.java
+++ heroes-backend/src/main/java/heroes/backend/HeroRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Repository for Heroes
  */
 public interface HeroRepository extends JpaRepository<Hero, Integer> {
+    Iterable<Hero> findAllByOrderByName();
 }
