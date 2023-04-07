3534fbe4a6b66440fa25c1bb5d344eac711e824e
Christian Köberl <christian.koeberl@porscheinformatik.com>
2023-04-07 10:28:17 +0200

    Fix changeset hash computation
    See https://github.com/quarkusio/quarkus/issues/15809

--- heroes-webapp/src/main/java/heroes/nativehints/LiquibaseNativeHints.java
+++ heroes-webapp/src/main/java/heroes/nativehints/LiquibaseNativeHints.java
@@ -1,7 +1,8 @@
 package heroes.nativehints;
 
 import java.lang.reflect.Constructor;
-
+import liquibase.change.core.CreateIndexChange;
+import liquibase.change.core.CreateTableChange;
 import org.springframework.aot.hint.ExecutableMode;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
@@ -13,18 +14,21 @@ public class LiquibaseNativeHints implements RuntimeHintsRegistrar {
 
   @Override
   public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
-     registerConstructor(hints, liquibase.sql.visitor.AppendSqlVisitor.class);
-     registerConstructor(hints, liquibase.sql.visitor.PrependSqlVisitor.class);
-     registerConstructor(hints, liquibase.sql.visitor.RegExpReplaceSqlVisitor.class);
-     registerConstructor(hints, liquibase.sql.visitor.ReplaceSqlVisitor.class);
-     registerConstructor(hints, java.util.ArrayList.class);
+    registerConstructor(hints, liquibase.sql.visitor.AppendSqlVisitor.class);
+    registerConstructor(hints, liquibase.sql.visitor.PrependSqlVisitor.class);
+    registerConstructor(hints, liquibase.sql.visitor.RegExpReplaceSqlVisitor.class);
+    registerConstructor(hints, liquibase.sql.visitor.ReplaceSqlVisitor.class);
+    registerConstructor(hints, java.util.ArrayList.class);
 
-     registerPublicMethods(hints, liquibase.change.core.AlterSequenceChange.class);
-     registerPublicMethods(hints, liquibase.change.core.CreateIndexChange.class);
-     registerPublicMethods(hints, liquibase.change.core.CreateProcedureChange.class);
-     registerPublicMethods(hints, liquibase.change.core.CreateSequenceChange.class);
-     registerPublicMethods(hints, liquibase.change.core.CreateViewChange.class);
-     registerPublicMethods(hints, liquibase.change.core.DeleteDataChange.class);
+    registerPublicMethods(hints, liquibase.change.core.AlterSequenceChange.class);
+    registerPublicMethods(hints, liquibase.change.core.CreateIndexChange.class);
+    registerPublicMethods(hints, liquibase.change.core.CreateProcedureChange.class);
+    registerPublicMethods(hints, liquibase.change.core.CreateSequenceChange.class);
+    registerPublicMethods(hints, liquibase.change.core.CreateViewChange.class);
+    registerPublicMethods(hints, liquibase.change.core.DeleteDataChange.class);
+
+    hints.reflection().registerType(CreateIndexChange.class);
+    hints.reflection().registerType(CreateTableChange.class);
 
     hints.resources().registerPattern("db/changelog/*.xml");
   }
