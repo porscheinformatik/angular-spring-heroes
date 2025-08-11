da4d556016bc4867bc1827be646bd541702b4031
Christian Köberl <christian.koeberl@porscheinformatik.com>
2025-08-11 11:58:38 +0200

    chore: remove Stagewise

--- heroes-frontend/src/main.ts
+++ heroes-frontend/src/main.ts
@@ -4,15 +4,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { initToolbar } from '@stagewise/toolbar';
-import AngularPlugin from '@stagewise-plugins/angular';
 
 if (environment.production) {
   enableProdMode();
-} else {
-  initToolbar({
-    plugins: [AngularPlugin],
-  });
 }
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
